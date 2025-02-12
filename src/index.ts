01b0dd280930b6bbd801db378caa62c08b7b2453
carlhicban <68343524+carlhicban@users.noreply.github.com>
2025-02-12 22:03:50 +0800

    Update index ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello')
+import { User } from "./User"
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+/// <reference types="@types/google.maps" />
+
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+ 
+customMap.addMarker(user)
+customMap.addMarker(company)
+console.log(user)
+console.log(company)
+ 
